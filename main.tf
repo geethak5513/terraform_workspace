5a9fcd5040f53014ae9f7fbcc7d1190a247ade93
geethak5513 <41056499+geethak5513@users.noreply.github.com>
2026-09-19 13:46:54 +0530

    Migrate resources from Azure to AWS
    Replaced Azure resources with AWS equivalents, including VPC, subnet, and security group.

--- main.tf
+++ main.tf
@@ -1,21 +1,39 @@
-resource "azurerm_resource_group" "rg1" {
-  name     = "myresource_group"
-  location = "West Europe"
-}
-resource "azurerm_virtual_network" "vn1" {
-  name                = "myvirtual_network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg1.location
-  resource_group_name = azurerm_resource_group.rg1.name
-}
-resource "azurerm_subnet" "sbn1" {
-  name                 = "mysubnet"
-  resource_group_name  = azurerm_resource_group.rg1.name
-  virtual_network_name = azurerm_virtual_network.vn1.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-resource "azurerm_virtual_machine" "vm1" {
-  name                  = "myvirtualmachine"
-  location              = azurerm_resource_group.rg1.location
-  resource_group_name   = azurerm_resource_group.rg1.name
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-aws-vpc"
   }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "my-public-subnet"
+  }
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow inbound web traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
