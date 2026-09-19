10a13cf94635aef8759e847132dfcab91d39513b
geethak5513 <41056499+geethak5513@users.noreply.github.com>
2026-09-19 13:46:26 +0530

    Replace azurerm provider with aws provider

--- providers.tf
+++ providers.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
-provider "azurerm" {
-  features {}
-  client_id       = "00000000-0000-0000-0000-000000000000"
-  client_secret   = var.client_secret
-  tenant_id       = "9a8cd433-6113-49e5-aa7f-42788a01a246"
-  subscription_id = "20000000-0000-0000-0000-000000000000"
+
+provider "aws" {
+  region = "us-east-1" # Match the region in your workflow file
 }
